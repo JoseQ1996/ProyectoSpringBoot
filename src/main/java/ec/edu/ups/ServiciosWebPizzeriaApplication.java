524f46dbaa0e7b35007aef06a56ac224ef95b98a
José Quinde <49071271+JoseQ1996@users.noreply.github.com>
2022-07-21 11:40:36 -0500

    proyecto springboot

---
+++ src/main/java/ec/edu/ups/ServiciosWebPizzeriaApplication.java
@@ -0,0 +1,14 @@
+package ec.edu.ups;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@ComponentScan("ec.edu.ups")
+public class ServiciosWebPizzeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiciosWebPizzeriaApplication.class, args);
+	}
+
+}
